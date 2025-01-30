2de6af46ed8b6d2b0dddee4ee53c3490cda98745
NavinChoudhary34 <navin.r.choudharylj@gmail.com>
2025-01-30 09:59:21 +0530

    Update Visions.jsx

--- src/components/home/Visions.jsx
+++ src/components/home/Visions.jsx
@@ -5,15 +5,20 @@ export default function Visions() {
         <div id="vision"className={styles.visions}>
             <div className={styles.visionItem}>
                 <h2>Explore</h2>
-                <p>Explore cutting-edge technologies and enhance your skills with expert-led sessions and resources.</p>
+                <p>Dive into the world of cutting-edge tech education through our expertly curated workshops, hackathons, and speaker sessions.
+                   Whether it’s learning the fundamentals of cloud computing, mastering machine learning, or understanding the latest in web development, GDG-PCE provides hands-on experiences that make complex concepts accessible to all.</p>
             </div>
             <div className={styles.visionItem}>
                 <h2>Engage</h2>
-                <p>Network with like-minded developers and technologists from diverse backgrounds.</p>
+                <p>Expand your network by connecting with peers, tech leaders, and professionals.
+                   GDG-PCE offers a unique platform to collaborate with like-minded individuals, participate in peer-to-peer learning, and brainstorm innovative solutions for real-world challenges.
+                   Build lasting relationships that can help shape your journey as a future-ready developer.</p>
             </div>
             <div className={styles.visionItem}>
                 <h2>Evolve</h2>
-                <p>Advance your skills and career by leveraging community-driven opportunities.</p>
+                <p>Take your technical abilities to the next level with study jams, webinars, and the annual Solution Challenge competition.
+                    Learn to design, develop, and implement impactful solutions that address local and global problems.
+                    Gain hands-on experience while earning the opportunity to win exciting prizes from Google.</p>
             </div>
         </div>
     );
