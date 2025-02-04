c0b9ac687316a6a2e60ff46d7bbaa983db5dde9d
Atharva Kolekar <118675133+AtharvaKolekar@users.noreply.github.com>
2025-02-04 21:54:46 +0530

    added arrow as pointer

--- src/app/layout.js
+++ src/app/layout.js
@@ -40,7 +40,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0, shrink-to-fit=no, viewport-fit=cover" />
             </head>
             <body className={googleSans.className}>
                 <NextTopLoader style={{ zIndex: 500 }} />
